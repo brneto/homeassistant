f5978779026257ce1b5c8df67f86b9b7d042111b
Berchris Requiao <brneto@gmail.com>
2023-01-11 16:36:44 +0000

    Move ha container volume

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -2,19 +2,31 @@
 set -ex
 
 # Copy the configuration into HA container
-while [ ! -d /opt/homeassistant/config/.storage ]
-do
-  sleep 1
-done
-cp -r config/. /opt/homeassistant/config/.
+cp_initial_config () {
+  while [ ! -d /opt/homeassistant/config/.storage ]
+  do
+    sleep 1
+  done
+  cp -r config/. /opt/homeassistant/config/.
+}
 
-# Add HACS addon
-if [ ! -d /opt/homeassistant/config/custom_components/hacs ]
-then
-  (cd /opt/homeassistant/config && wget -O - https://get.hacs.xyz | bash -)
-fi
+# Restart Home Assistant container
+restart_ha () {
+  for i in $(docker ps | awk 'NR>1 {print $(NF)}')
+  do
+    [ "homeassistant" = "$i" ] && docker restart "$i" && break
+  done
+}
 
-for i in $(docker ps | awk 'NR>1 {print $(NF)}')
-do
-  [ "homeassistant" = "$i" ] && docker restart "$i"
-done
+# Install HACS addon
+install_hacs () {
+  if [ ! -d /opt/homeassistant/config/custom_components/hacs ]
+  then
+    (cd /opt/homeassistant/config && wget -O - https://get.hacs.xyz | bash -)
+    restart_ha
+  fi
+}
+
+
+cp_initial_config
+install_hacs
